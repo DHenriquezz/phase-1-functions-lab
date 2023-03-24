21e678c97b71ed8d164ab8604e87179b44acb24c
Douglas Flores <Dflores5df@icloud.com>
2023-03-24 17:04:22 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks){
+    const hq = 42;
+    return Math.abs(blocks - hq)
+}
+
+function distanceFromHqInFeet(blocks){
+  const feet = distanceFromHqInBlocks(Math.abs((blocks - 42) * 264)+ 42);
+  return feet;
+}
+function distanceTravelledInFeet(start, destination){
+    const block = 264;
+    return Math.abs((start - destination) * block)
+}
+function calculatesFarePrice(start, destination) {
+     const distance = distanceTravelledInFeet(start, destination);
+     const freeRide = 400;
+        let price;
+        if (distance <= freeRide){
+            price = 0;
+        } else if (distance > 400 && distance < 2000){
+            price = (distance - freeRide) * 0.02;
+        } else if (distance > 2000 && distance < 2500){
+            price = 25;
+        } else 
+            price = "cannot travel that far"
+        
+        return price;
+    }
